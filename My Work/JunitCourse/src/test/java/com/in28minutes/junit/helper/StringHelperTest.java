95d9185947ead10170ecdf7d5f4904aa512df177
Limagreen <89492602+Limagreen@users.noreply.github.com>
2023-08-07 17:00:39 -0400

    Step 04 : Refactoring Your First JUnit Test

--- My Work/JunitCourse/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ My Work/JunitCourse/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -9,12 +9,17 @@ public class StringHelperTest {
 	private StringHelper helper = new StringHelper();
 	
 	@Test
-	public void test() {
+	public void testTruncateAInFirst2Positions_AInFirst2Positions () {
 		
+		// variables instead of inline arguments for demonstrative purposes
 		String expected = "CD";
 		String actual = helper.truncateAInFirst2Positions("AACD");
 		
 		assertEquals(expected, actual);
 	}
 
+	@Test
+	public void testTruncateAInFirst2Positions_AInFirstPosition () {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
 }
