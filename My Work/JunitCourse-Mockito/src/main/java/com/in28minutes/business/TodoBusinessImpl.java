df549fb2c9a18728a6f172cbd8c4a61a18cf2d25
Limagreen <89492602+Limagreen@users.noreply.github.com>
2023-08-17 11:49:38 -0400

    Mockito Step 02 : Setting up SUT (System Under Test)

---
+++ My Work/JunitCourse-Mockito/src/main/java/com/in28minutes/business/TodoBusinessImpl.java
@@ -0,0 +1,33 @@
+package com.in28minutes.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.in28minutes.data.api.TodoService;
+
+// TodoBusinessImpl is our SystemUnderTest (SUT)
+// TodoService is a Dependency required by that SUT
+
+public class TodoBusinessImpl {
+	
+	private TodoService todoService;
+
+	// constructor
+	TodoBusinessImpl(TodoService todoService) {
+		this.todoService = todoService;
+	}
+	
+	public List<String> retrieveTodosRelatedToSpring(String user){
+		
+		List<String> filteredTodos = new ArrayList<String>();
+		
+		// for the purposes of this work, we pretend retrieveTodos fetches data from an API
+		for (String todo : todoService.retrieveTodos(user)) {
+			if(todo.contains("Spring")) {
+				filteredTodos.add(todo);
+			}
+		}
+		
+		return filteredTodos;
+	}
+}
